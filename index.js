3d71b04c036f9edfaaaedccad945700ac7ae19d5
T-Streets <tyler.ceja5@gmail.com>
2018-06-06 18:03:41 -0700

    creates user model class

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express();
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/user');
+
+mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
+const app = express();
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
