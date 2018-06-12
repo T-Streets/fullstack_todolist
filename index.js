6df5893476c68e8ccb26ad822717176f71230e2d
T-Streets <tyler.ceja5@gmail.com>
2018-06-12 13:04:17 -0700

    creates logout endpoint

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-require('./routes/authRoutes')(app);
 
 /**
  * sets max age of cookie and encrypts it
@@ -26,6 +25,8 @@ app.use(
  */
 app.use(passport.initialize());
 app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
