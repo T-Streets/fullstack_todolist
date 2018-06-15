e40d209bdfb7a76abf308f82a602925099193a34
T-Streets <tyler.ceja5@gmail.com>
2018-06-15 11:13:35 -0700

    configures redux reducers and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoApp from './Components/TodoApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Write It Down. Get It Done</h1>
+          <h1 className="App-title">Write It Down. Get It Done.</h1>
         </header>
         <TodoApp />
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './Reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
