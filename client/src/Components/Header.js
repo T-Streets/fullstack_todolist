6f729fd1d3d704136c1a102f3aabedd09a3c7bd5
T-Streets <tyler.ceja5@gmail.com>
2018-06-16 20:55:03 -0700

    configures header to respond to auth

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href='/auth/google'>Login</a></li>
+            default:
+                return <li><a href='/api/logout'>Logout</a></li>
+        }
+    }
     render() {
         return (
             <div>
                 <h1>write it down. get it done.</h1>
                 <ul>
-                    <li><a>Login</a></li>
-                    <li><a>Logout</a></li>
+                    {this.renderContent()}
                 </ul>
             </div>
         )
     }
 }
+
+function mapStateToProps({ auth }) {
+    return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/Reducers/authReducer.js
+++ client/src/Reducers/authReducer.js
@@ -4,7 +4,6 @@ import { FETCH_USER } from '../Actions/types';
  * Returns the state of user authentication
  */
 export default function(state = null, action) {
-    console.log(action)
     switch(action.type) {
         case FETCH_USER:
             return action.payload || false;
